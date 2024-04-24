9ebc0502da7fae26392148f3f17bdfad799b6b4c
g <gurwindersingh00064@gmail.com>
2024-04-24 10:36:07 +0530

    zivoe

---
+++ Test/testDao.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+
+
+import "../src/ZivoeDAO.sol";
+
+contract ZivoeDAOTest is Test {
+
+    function setUp() public {
+           address GBL = new ZivoeDAO();
+    }
+
+    function test_push()
+}
